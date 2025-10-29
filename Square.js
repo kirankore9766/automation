92ef45ecdb4ed2c68f4f6c674acd01ac66988601
kirankore9766 <kiran.kore@cointab.net>
2025-10-29 20:22:37 +0530

    Update Square.js

--- Square.js
+++ Square.js
@@ -3,3 +3,7 @@ const number = prompt('Enter the number: ');
 
 const result = Math.sqrt(number);
 console.log(`The square root of ${number} is ${result}`);
+console.log(`The square root of ${number} is ${result}`);
+console.log(`The square root of ${number} is ${result}`);
+
+
