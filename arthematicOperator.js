ffa5154d36637c919dd91929151a724a7017f6bd
kirankore9766 <kiran.kore@cointab.net>
2025-10-29 18:13:47 +0530

    Update arthematicOperator.js

--- arthematicOperator.js
+++ arthematicOperator.js
@@ -3,11 +3,16 @@
 //take input from the users
 let a = parseInt(prompt('Enter the first variable: '));
 let b = parseInt(prompt('Enter the second variable: '));
+let c = parseInt(prompt('Enter the third variable: '));
 
 // addition and subtraction operator
 a = a + b;
 b = a - b;
 a = a - b;
+c=a+b;
 
 console.log(`The value of a after swapping: ${a}`);
 console.log(`The value of b after swapping: ${b}`);
+console.log(`The value of c after swapping: ${c}`);
+
+
