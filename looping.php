27811e8d249355a9b0a491bec8c9cf19ea96efc9
kirankore9766 <kiran.kore@cointab.net>
2025-12-26 17:02:41 +0530

    Create looping.php

---
+++ looping.php
@@ -0,0 +1,11 @@
+<?php
+$number = 15;
+
+if ($number > 10) {
+    echo $number . " is greater than 10";
+} else {
+    echo $number . " is less than or equal to 10";
+  else{
+}
+// Output: 15 is greater than 10
+?>
